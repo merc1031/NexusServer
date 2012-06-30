101d2875f328226834f6746f02d587f5388b6e94
merc1031 <merc1031@gmail.com>
2012-06-30 14:39:25 -0700

    Forgot to include request module

--- NexusServer.py
+++ NexusServer.py
@@ -1,5 +1,5 @@
 from gevent import monkey; monkey.patch_all()
-from bottle import route, run, post
+from bottle import route, run, post, request
 
 
 @post('/file')
@@ -10,6 +10,7 @@ def postFile():
         data = request.json
         src = data.get('source', None)
         dest = data.get('destination', None)
+        return { "success" : True, "data" : { }, "code" : -1 }
 
 if __name__ == "__main__":
     run(host='localhost', port=24900, server='gevent')
