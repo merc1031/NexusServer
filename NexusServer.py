ce2cef4da54f12d59e61e72ca0428bf541cc83f6
merc1031 <merc1031@gmail.com>
2012-07-01 05:01:11 -0700

    server can now accept options

--- NexusServer.py
+++ NexusServer.py
@@ -13,26 +13,61 @@ def postFile():
     else:
         data = request.json
         print data
-        handleData(data)
+        global gHandler
+        gHandler.handleData(data)
 
         return { "success" : True, "data" : { }, "code" : -1 }
 
-def handleData(downloadItem):
-    import re
-    source = downloadItem['url']
-    dest = downloadItem['filename']
-    arrayDir = dest.split(os.sep)
-    if os.name == 'nt':
-        arrayDir[0] = arrayDir[0] + '\\' if arrayDir[0][1] == ':' else arrayDir[0]
-    destPath = os.path.join(*arrayDir)
-    (head, tail) = os.path.split(destPath)
-    for i in ['google','sourceforge']: 
-        o = re.search(i, source)
-        if o is not None:
-            tarDir = os.path.join(head, i)
-            mkdir_p(tarDir)
-            shutil.move(destPath, os.path.join(tarDir, tail) )
-            break
+@post('/opt')
+def postOptions():
+    if request.json is None:
+        abort(500)
+    else:
+        data = request.json
+        print data
+        print 'where my data'
+        global gHandler
+        gHandler.handleOptions(data)
+
+        return { "success" : True, "data" : { }, "code" : -1 }
+
+class Handler(object):
+    def __init__(self):
+        self.routes = {}
+
+    @staticmethod
+    def convertFromJS(options):
+        for option, value in options.iteritems():
+            value['enabled'] = True if value['enabled'] == 'on' else False
+
+        return options 
+
+    def handleOptions(self, options):
+        self.routes = Handler.convertFromJS(options)
+
+    def handleData(self, downloadItem):
+        import re
+        source = downloadItem['url']
+        dest = downloadItem['filename']
+        destPath = Handler.fixPath(dest)
+        (head, tail) = os.path.split(destPath)
+        for k,v in self.routes.iteritems(): 
+            o = re.search(k, source)
+            if o is not None:
+                tarDir = Handler.fixPath(v['route']) 
+                mkdir_p(tarDir)
+                shutil.move(destPath, os.path.join(tarDir, tail) )
+                break
+
+    @staticmethod
+    def fixPath(path):
+        arrayDir = path.split(os.sep)
+        if os.name == 'nt':
+            arrayDir[0] = arrayDir[0] + '\\' if arrayDir[0][1] == ':' else arrayDir[0]
+        destPath = os.path.join(*arrayDir)
+        return destPath
+
+gHandler = None
 
 def mkdir_p(path):
     try:
@@ -43,4 +78,6 @@ def mkdir_p(path):
         else: raise
 
 if __name__ == "__main__":
+    gHandler = Handler()
+
     run(host='localhost', port=24900, server='gevent')
